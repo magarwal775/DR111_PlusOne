b6af9b36a904bff5d99c7e1ad7b4bc11eec69e10
Chirag Agarwal <chirag@gamil.com>
2020-08-01 15:10:40 +0530

    [ADD] views and forms for usermodel

---
+++ accounts/forms.py
@@ -0,0 +1,202 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.db import transaction
+import datetime
+from college.models import College, Department, Course, Specialization
+from accounts.models import Alumni, Faculty, User
+from PIL import Image
+
+def year_choices():
+    return [(r,r) for r in range(1947, datetime.date.today().year+1)]
+
+class AlumniSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    college = forms.ModelChoiceField(queryset=College.objects.all(), required=True)
+    unique_id = forms.CharField(max_length=200)
+    email = forms.EmailField()
+    image = forms.ImageField()
+    phone = forms.CharField(max_length=20)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_alumni = True
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.full_name = self.cleaned_data['first_name'] + " " + self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.college = College.objects.get(name=self.cleaned_data['college'])
+        user.profile_photo = self.cleaned_data['image']
+        user.phone = self.cleaned_data['phone']
+        user.save()
+        unique_id = self.cleaned_data['unique_id']
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        email_address = self.cleaned_data['email']
+        system_date_joined = datetime.datetime.now()
+
+        alumni = Alumni.objects.create(
+            user=user,
+            unique_id=unique_id,
+            )
+        return user
+
+class FacultySignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    college = forms.ModelChoiceField(queryset=College.objects.all(), required=True)
+    email = forms.EmailField()
+    image = forms.ImageField()
+    phone = forms.CharField(max_length=20)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_faculty = True
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.full_name = self.cleaned_data['first_name'] + " " + self.cleaned_data['last_name']
+        user.college = College.objects.get(name=self.cleaned_data['college'])
+        user.profile_photo = self.cleaned_data['image']
+        user.phone = self.cleaned_data['phone']
+        user.save()
+        system_date_joined = datetime.datetime.now()
+
+        faculty = Faculty.objects.create(
+            user=user,
+            )
+        return user
+
+class CompleteAlumniProfile(forms.ModelForm):
+
+    course_choices = Course.objects.all()
+    department_choices = Department.objects.all()
+    specialization_choices = Specialization.objects.all()
+
+    course = forms.ModelChoiceField(queryset=course_choices, required=True)
+    department = forms.ModelChoiceField(queryset=department_choices, required=False)
+    specialization = forms.ModelChoiceField(queryset=specialization_choices, required=False)
+    dob = forms.DateField()
+    year_of_passing = forms.ChoiceField(choices=year_choices())
+    location = forms.CharField(max_length=200, required=True)
+    facebook_profile = forms.URLField(max_length=1000, required=False)
+    twitter_profile = forms.URLField(max_length=1000, required=False)
+    linkedin_profile = forms.URLField(max_length=1000, required=False)
+    about_me = forms.CharField(max_length=1000, widget=forms.TextInput({}), required=False)
+
+    class Meta:
+        model = Alumni
+        fields = ('dob', 'course', 'department', 'specialization', 'location', 'facebook_profile', 'twitter_profile','linkedin_profile','about_me')
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(CompleteAlumniProfile, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        user = self.user
+        alumni = Alumni.objects.get(user=self.user)
+
+        department = self.cleaned_data['department']
+        if department:
+            user.department = Department.objects.get(name=self.cleaned_data['department'])
+
+        course = self.cleaned_data['course']
+        if course:
+            user.course = Course.objects.get(name=self.cleaned_data['course'])
+
+        specialization = self.cleaned_data['specialization']
+        if specialization:
+            user.specialization = Specialization.objects.get(name=self.cleaned_data['specialization'])
+
+        user.dob = self.cleaned_data['dob']
+        alumni.year_of_passing = self.cleaned_data['year_of_passing']
+        user.facebook_profile = self.cleaned_data['facebook_profile']
+        user.twitter_profile = self.cleaned_data['twitter_profile']
+        user.linkedin_profile = self.cleaned_data['linkedin_profile']
+        user.about_me = self.cleaned_data['about_me']
+        user.location = self.cleaned_data['location']
+        user.profile_complete=1
+        alumni.save()
+        user.save()
+
+class CompleteFacultyProfile(forms.ModelForm):
+
+    course_choices = Course.objects.all()
+    department_choices = Department.objects.all()
+    specialization_choices = Specialization.objects.all()
+
+    course = forms.ModelChoiceField(queryset=course_choices, required=True)
+    department = forms.ModelChoiceField(queryset=department_choices, required=False)
+    specialization = forms.ModelChoiceField(queryset=specialization_choices, required=False)
+    dob = forms.DateField()
+    college_joined_year = forms.ChoiceField(choices=year_choices())
+    research_interest = forms.CharField(max_length=300)
+    facebook_profile = forms.URLField(max_length=1000, required=False)
+    twitter_profile = forms.URLField(max_length=1000, required=False)
+    linkedin_profile = forms.URLField(max_length=1000, required=False)
+    about_me = forms.CharField(max_length=1000, widget=forms.TextInput({}), required=False)
+
+    class Meta:
+        model = Alumni
+        fields = ('dob', 'department', 'college_joined_year', 'research_interest')
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(CompleteFacultyProfile, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        user = self.user
+        faculty = Faculty.objects.get(user=self.user)
+
+        department = self.cleaned_data['department']
+        if department:
+            user.department = Department.objects.get(name=self.cleaned_data['department'])
+
+        course = self.cleaned_data['course']
+        if course:
+            user.course = Course.objects.get(name=self.cleaned_data['course'])
+
+        specialization = self.cleaned_data['specialization']
+        if specialization:
+            user.specialization = Specialization.objects.get(name=self.cleaned_data['specialization'])
+
+        user.dob = self.cleaned_data['dob']
+        faculty.college_joined_year = self.cleaned_data['college_joined_year']
+        faculty.research_interest = self.cleaned_data['research_interest']
+        user.facebook_profile = self.cleaned_data['facebook_profile']
+        user.twitter_profile = self.cleaned_data['twitter_profile']
+        user.linkedin_profile = self.cleaned_data['linkedin_profile']
+        user.about_me = self.cleaned_data['about_me']
+        user.profile_complete=1
+        faculty.save()
+        user.save()
+
+class AccountAuthenticationForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def clean(self):
+        if self.is_valid():
+            email=self.cleaned_data['email']
+            password=self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid Login")
+
+class UpdateAlumniProfile(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'college', 'dob')
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,141 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.urls import reverse
+from django.http import HttpResponse
+from accounts.forms import (
+    AlumniSignUpForm,
+    FacultySignUpForm,
+    AccountAuthenticationForm,
+    CompleteAlumniProfile,
+    CompleteFacultyProfile,
+    UpdateAlumniProfile,
+)
+from django.views.generic import CreateView, ListView, UpdateView
+from accounts.models import User
+from django_email_verification import sendConfirm
+from django.conf import settings
+from django_email_verification.Confirm import verifyToken
+from django_email_verification.errors import NotAllFieldCompiled
 
-# Create your views here.
+
+class alumni_signup_view(CreateView):
+    model = User
+    form_class = AlumniSignUpForm
+    template_name = "account/signup_form.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["user_type"] = "Alumni"
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        sendConfirm(user)
+        return redirect("accounts:complete_alumni_profile")
+
+
+class faculty_signup_view(CreateView):
+    model = User
+    form_class = FacultySignUpForm
+    template_name = "account/signup_form.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["user_type"] = "Faculty"
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        sendConfirm(user)
+        return redirect("accounts:complete_faculty_profile")
+
+
+def signup_view(request):
+    return render(request, "account/signup.html")
+
+
+def complete_alumni_profile(request):
+
+    context = {}
+
+    user = request.user
+    if not user.is_verified:
+        return render(request, "account/please_verify.html", context)
+
+    if request.POST:
+        form = CompleteAlumniProfile(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("base:home")
+
+    else:
+        form = CompleteAlumniProfile(request.user)
+
+    context["form"] = form
+    return render(request, "account/complete_alumni_profile.html", context)
+
+
+def complete_faculty_profile(request):
+    context = {}
+
+    user = request.user
+    if not user.is_verified:
+        return render(request, "account/please_verify.html", context)
+
+    if request.POST:
+        form = CompleteFacultyProfile(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("base:home")
+
+    else:
+        form = CompleteFacultyProfile(request.user)
+
+    context["form"] = form
+    return render(request, "account/complete_faculty_profile.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("base:home")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect("base:home")
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username, password))
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, "account/login.html")
+
+
+def update_alumni_profile(request):
+
+    context = {}
+
+    user = request.user
+
+    if request.POST:
+        form = UpdateAlumniProfile(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("base:home")
+    else:
+        form = UpdateAlumniProfile(instance=request.user)
+
+    context["form"] = form
+    return render(request, "account/editprofile.html", context)
+
+
+def verify(request):
+    return render(request, "account/please_verify.html", {})
